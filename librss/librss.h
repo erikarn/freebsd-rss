f575941d3b86e67f86fd4bc40d140877893cf92f
Adrian Chadd <adrian@freebsd.org>
2014-09-12 20:31:19 +0000

    Add some comments about what these functions do.

--- librss/librss.h
+++ librss/librss.h
@@ -1,7 +1,19 @@
 #ifndef	__LIBRSS_H__
 #define	__LIBRSS_H__
 
+/*
+ * Enable/disable whether to allow for multiple bind()s to the
+ * given PCB entry.
+ *
+ * This must be done before bind().
+ */
 extern	int rss_sock_set_bindmulti(int fd, int af_family, int val);
+
+/*
+ * Set the RSS bucket for the given file descriptor.
+ *
+ * This must be done before bind().
+ */
 extern	int rss_sock_set_rss_bucket(int fd, int af_family, int rss_bucket);
 
 /*
@@ -10,7 +22,16 @@ extern	int rss_sock_set_rss_bucket(int fd, int af_family, int rss_bucket);
  */
 extern	int rss_sock_set_recvrss(int fd, int af_family, int val);
 
+/*
+ * Generic "retrive the int value for the given sysctl"
+ * worker function.
+ */
 extern	int rss_getsysctlint(const char *s);
+
+/*
+ * Retrieve the mapping between RSS bucket and
+ * CPU ID.
+ */
 extern	int rss_getbucketmap(int *bucket_map, int nbuckets);
 
 #endif /* __LIBRSS_H__ */
